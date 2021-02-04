23e6a755125076feae255eadcfd0e3c25d5e6ee3
nikosbalta <78555365+nikosbalta@users.noreply.github.com>
2021-02-04 17:21:30 +0000

    Create dotProd.py
    A demo of using dot product in numpy

---
+++ dotProd.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 17:11:32 2021
+
+@author: nikos
+"""
+
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+
+print(np.dot(a,b))
+
